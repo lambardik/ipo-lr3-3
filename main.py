71628ea1b559c09e371a898e1bfe3c110807c239
даниэль <dannilboyar@gmail.com>
2024-10-01 10:14:25 +0300

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 day = int(input("Введите день: "))
 month = int(input("Введите месяц: "))
-while month < 1 or month > 12:
+
+if month < 1 or month > 12:
     print("Некорректный месяц. Введите число от 1 до 12.")
     month = int(input("Введите месяц: "))
-while day < 1 or day > 31:
+
+if day < 1 or day > 31:
     print("Некорректный день. Введите число от 1 до 31.")
     day = int(input("Введите день: "))
+
+if month == 2 and day > 29:
+    print("В феврале может быть до 29 дней в високосный год. Введите число от 1 до 29.")
+    day = int(input("Введите день: "))
+
 if 3 <= month <= 5:
     season = "Весна"
 elif 6 <= month <= 8:
@@ -15,4 +22,4 @@ elif 9 <= month <= 11:
 else:
     season = "Зима"
 
-print(f"Дата {day}.{month} относится к {season}.") 
+print(f"Дата {day}.{month} относится к {season}.")
